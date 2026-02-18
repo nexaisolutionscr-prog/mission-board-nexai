635fcf43bf8cfde8fceaf050d1abf22b4b132359
root <root@srv1360772.hstgr.cloud>
2026-02-18 05:42:58 +0000

    fix: import ProgressBar and define tasks in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import KanbanBoard from '../components/KanbanBoard';
+import ProgressBar from '../components/ProgressBar';
+import { loadTasks } from '../utils/localStorageUtils';
 
-const Home = () => (
-  <div className='container mx-auto p-4'>
-    <h1 className='text-center text-lg font-bold'>Mission Board</h1>
-    <KanbanBoard />
-    <ProgressBar progress={tasks.filter(task => task.status === 'Done').length / tasks.length * 100} />
-  </div>
-);
+const Home = () => {
+  const tasks = loadTasks();
+  const doneTasks = tasks ? Object.values(tasks).flat().filter(t => t.status === 'Done').length : 0;
+  const totalTasks = tasks ? Object.values(tasks).flat().length : 0;
+  const progress = totalTasks > 0 ? (doneTasks / totalTasks) * 100 : 0;
+
+  return (
+    <div className='container mx-auto p-4'>
+      <h1 className='text-center text-lg font-bold'>Mission Board</h1>
+      <ProgressBar progress={progress} />
+      <KanbanBoard />
+    </div>
+  );
+};
 
 export default Home;
