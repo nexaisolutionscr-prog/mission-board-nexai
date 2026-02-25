f01815b67a984cc22a62d6d81abc839a641d9433
Jose Barrantes <jose@nexaisolutionscr.com>
2026-02-25 16:42:06 -0500

    Fix: Add Firebase storage export

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAHUr4ihWK3pXzfFY4rVT-Qmh5KFBWnXtA",
@@ -14,5 +15,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, db };
+export { app, db, storage };
